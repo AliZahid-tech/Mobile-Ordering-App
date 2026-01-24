6974de28ebed060422295768002c5c74f3534403
AliZahid-tech <alizahidkhan822@gmail.com>
2026-01-25 04:53:02 +0500

    REVAMPING

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 import menuArray from "/data.js";
 // const addBtn = document.querySelectorAll(".add");
-const orderSec = document.getElementById("your-order");
+const orderSec = document.getElementById("cart-items");
 let orderSecIsClosed = orderSec.classList.contains("hidden");
 let cartArr = [];
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("add")) {
-    const currItem = e.target.parentElement;
-    handleAddToCart(cartArr, currItem);
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.btn) {
+    const id = Number(e.target.dataset.btn);
+    console.log(id);
+    renderOrderCart(id);
   }
 });
-function handleAddToCart(cartArr, crrItem) {
-  const product = {
-    icon: crrItem.querySelector(".item-icon").textContent,
-    name: crrItem.querySelector(".item-name").textContent,
-    price: parseInt(crrItem.querySelector(".item-price").textContent),
-    ingredients: crrItem.querySelector(".item-ingr").textContent,
-  };
-  if (orderSecIsClosed) {
-    orderSec.classList.remove("hidden");
-    orderSecIsClosed = false;
-  }
-  cartArr.push(product);
-  console.log(cartArr);
+let menuSec = document.getElementsByClassName("menu-data")[0];
+function getMenuHtml() {
+  let menuHtml = ``;
+  menuArray.forEach(function (menuItem) {
+    menuHtml += `
+    <div class="menu">
+        <p class="item-icon">${menuItem.emoji}</p>
+        <div class="item-info">
+          <p class="item-name">${menuItem.name}</p>
+          <p class="item-ingr">${menuItem.ingredients}</p>
+          <p>$<span class="item-price">${menuItem.price}</span></p>
+        </div>
+        <button class="add" data-btn ="${menuItem.id}" >+</button>
+      </div>
+    `;
+  });
+  menuSec.innerHTML = menuHtml;
 }
-// addBtn.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     orderSec.classList.toggle("hidden");
-//   });
-// });
+getMenuHtml();
+function renderOrderCart(btnId) {
+  let total = 0;
+  menuArray.forEach((item) => {
+    if (item.id === btnId) {
+      cartArr.push(item);
+    }
+  });
+  orderSec.innerHTML = ``;
+  cartArr.forEach((item) => {
+    orderSec.innerHTML += `
+
+      <section class="cart">
+        <div class="cart-item">
+          <div class="cart-item-info">
+            <p class="cart-item-name">${item.name}</p>
+            <p class="cart-item-remove">Remove</p>
+          </div>
+          <p>$ <span  class="cart-item-price">${item.price}</span></p>
+        </div>
+      </section>
+
+    `;
+  });
+  total = cartArr.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
+  document.getElementById("total-price").textContent = total;
+}
